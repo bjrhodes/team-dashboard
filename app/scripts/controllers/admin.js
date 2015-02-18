05073549c0bb4bec8dddb0e4ae2eceb4d682f781
Barry Rhodes <barry@insidefoundry.co.uk>
2015-02-18 23:38:45 +0000

    threw in a few forms that do nothing and go no-where!

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -7,7 +7,7 @@
  * # AboutCtrl
  * Controller of the easterdashApp
  */
-angular.module('easterdashApp').controller('AdminCtrl', function (teamDb) {
+angular.module('easterdashApp').controller('AdminCtrl', function ($scope, teamDb) {
     teamDb.put({
         _id: 'teams',
         data: [
@@ -17,4 +17,15 @@ angular.module('easterdashApp').controller('AdminCtrl', function (teamDb) {
             {name: 'Team #Ω', description: 'Doing some awesome stuff an\' that!', currentBalance:312}
         ]
     });
+
+    $scope.teams = [
+        {name: 'Team #A', description: 'Doing some awesome stuff an\' that!', currentBalance:123},
+        {name: 'Team #2', description: 'Doing some awesome stuff an\' that!', currentBalance:231},
+        {name: 'Team #III', description: 'Doing some awesome stuff an\' that!', currentBalance:213},
+        {name: 'Team #Ω', description: 'Doing some awesome stuff an\' that!', currentBalance:312}
+    ]
+
+    $scope.saveCost = function(costIncurred) { /* @todo */ };
+    $scope.savePayout = function(taskDetails) { /* @todo */ };
+    $scope.addTeam = function(teamDetails) { /* @todo */ };
 });
